ae5c47a4e8567bcf6c5b2f60b6e1e47e6719d894
averagengineer <me@danielshare.co.uk>
2021-08-15 20:49:00 +0100

    Initial blog

--- danielshare.co.uk/docusaurus.config.js
+++ danielshare.co.uk/docusaurus.config.js
@@ -3,102 +3,78 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Daniel Share\'s Blog',
+  tagline: 'My tech blog',
+  url: 'https://danielshare.co.uk',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'img/logo.png',
+  organizationName: 'Superbition', // Usually your GitHub org/user name.
+  projectName: 'blog', // Usually your repo name.
   themeConfig: {
+    announcementBar: {
+      id: 'announcementBar-1', // Increment on change
+      content:
+          'Automatic database, file and server backups and management: <a target="_blank" rel="noopener" href="https://bakup.io">Bakup.io</a>',
+    },
     navbar: {
-      title: 'My Site',
+      title: 'Daniel Share\'s Blog',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Blog Logo',
+        src: 'img/logo.png',
       },
       items: [
+        {to: '/', label: 'Blog', position: 'left'},
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
+          href: 'https://bakup.io',
+          label: 'Bakup.io',
+          position: 'right',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://twitter.com/danielashare',
+          label: 'Twitter',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/danielashare',
           label: 'GitHub',
           position: 'right',
         },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
     prism: {
       theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
+      additionalLanguages: ['php'],
     },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    metadatas: [
+      {
+        name: 'twitter:card', content: 'summary_large_image',
+      },
+      {
+        name: 'twitter:site', content: '@danielashare',
+      },
+      {
+        name: 'twitter:creator', content: '@danielashare',
+      },
+    ],
   },
   presets: [
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
+        docs: false,
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          path: './blog',
+          routeBasePath: '/',
+          blogTitle: 'Daniel Share\'s Blog',
+          blogDescription: 'A technical blog about building Bakup.io',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
